e5ca97a5cf709d1b7d193707e3ebafe3268b570c
VINAY KUMAR RAPAKA <107675897+Vinayrapaka1@users.noreply.github.com>
2022-07-06 15:18:14 +0530

    Add files via upload

---
+++ slicing-part2.py
@@ -0,0 +1,50 @@
+# a='python'
+
+# print(a[2])
+# print(a[4])
+# print(a[3])
+
+# print(a[ :3])
+# print(a[ :4])
+# print(a[ :2])
+# print(a[2:5])
+
+# print(a[-3])
+# print(a[-4])
+# print(a[-3])
+# print(a[-6])
+
+# print(a[-5:-2])
+# print(a[:-1])
+# print(a[:-4])
+# print(a[:-6])
+# print(a[:-7])
+
+# a='12345'
+# print(int(a[0])+int((a[-1])))
+# print(int(a[2])+int((a[-3])))
+# print(int(a[4])+int((a[-2]))) 
+
+# b='56789'
+# print(int(b[0])+int(b[-1]))
+
+# a='python'
+# print(a[ : : -1])
+# print(a[ : :-2 ])
+# print(a[2::5])
+# print(a[3::4])
+
+
+# print(a[1:5:2])
+# print(a[2:3:3])
+# print(a[2:4:1])
+
+# txt='36 is my age'
+# x=txt.capitalize()
+# print(x)
+
+txt="hello,and welcome to my world"
+x=txt.capitalize()
+print(x)
+
+
